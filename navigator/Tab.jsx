0160762cecab3b052c6d69955f9bff6517d89df5
hwlee <hwlee@spofeed.com>
2023-10-13 09:04:47 +0900

    etc: 간단한 코드 수

--- navigator/Tab.jsx
+++ navigator/Tab.jsx
@@ -1,18 +1,11 @@
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import React from "react";
 import { Ionicons } from "@expo/vector-icons";
-import Menu2 from "../screens/Menu2";
-import Menu3 from "../screens/Menu3";
-import Menu4 from "../screens/Menu4";
 import Home from "../screens/Home";
 import Ticket from "../screens/Ticket";
 import { useColorScheme } from "react-native";
 import Profile from "../screens/Profile";
-import { MyDrawer } from "./Drawer";
 import Notice from "../screens/Notice";
-import Test from "../screens/Test";
-import QrCreate from "../screens/qr/QrCreate";
-import QrScan from "../screens/qr/QrScan";
 import CustomTabBar from "../components/CustomTabBar";
 
 const Tab = createBottomTabNavigator();
